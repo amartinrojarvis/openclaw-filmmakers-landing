ecfb7d292d37297561d5e02a376314c52324a90c
Alberto Martin <a.martinro@gmail.com>
2026-04-17 08:08:17 +0200

    fix: CSP permite region1.google-analytics.com para GTM/GA4

--- next.config.ts
+++ next.config.ts
@@ -61,7 +61,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.stripe.com https://api.brevo.com https://api.kit.com https://www.google-analytics.com; frame-src https://www.youtube.com; media-src 'self';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.stripe.com https://api.brevo.com https://api.kit.com https://www.google-analytics.com https://region1.google-analytics.com; frame-src https://www.youtube.com; media-src 'self';",
           },
         ],
       },
