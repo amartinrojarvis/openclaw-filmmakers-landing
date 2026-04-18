c939302a82eb134c357b4e1862b8ab82336f5128
Alberto Martin <a.martinro@gmail.com>
2026-04-18 10:59:59 +0200

    fix: Meta Pixel en thank you page /gracias-prompts sin gating

--- app/gracias-prompts/page.tsx
+++ app/gracias-prompts/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { PageLayout } from '@/components/PageLayout';
 import { GraciasPromptsContent } from './GraciasPromptsContent';
+
+const META_PIXEL_ID = '4396076083961602';
 
 export const metadata: Metadata = {
   title: 'PDF en camino | IA para Filmmakers',
@@ -17,8 +20,29 @@ export const metadata: Metadata = {
 
 export default function GraciasPromptsPage() {
   return (
-    <PageLayout showBackButton={true} fullHeight={true}>
-      <GraciasPromptsContent />
-    </PageLayout>
+    <>
+      {/* Meta Pixel - sin gating para tracking de conversión */}
+      <Script
+        id="meta-pixel-gracias"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <PageLayout showBackButton={true} fullHeight={true}>
+        <GraciasPromptsContent />
+      </PageLayout>
+    </>
   );
 }
